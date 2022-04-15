2a9ea0a208b36421f1db0796489222b4c6280d0c
Michele Pozzi <123.mpozzi@gmail.com>
2022-04-15 23:38:57 +0200

    Add Taxonomy

---
+++ src/scripts/index.js
@@ -0,0 +1,15 @@
+scroll_container = document.getElementById("taxonomy-container");
+myID = document.getElementById("taxonomy-thresholding");
+
+/*
+var myScrollFunc = function() {
+  var y = window.scrollY;
+  if (y <= 400) {
+    myID.className = "show"
+  } else {
+    myID.className = "hide"
+  }
+};
+
+scroll_container.addEventListener("scroll", myScrollFunc);*/
+

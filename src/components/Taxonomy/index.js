3d4ede24f01139395822df23d196333d91d8b54a
Michele Pozzi <123.mpozzi@gmail.com>
2022-04-16 17:08:13 +0200

    Fix taxonomy. Fix layout

--- src/components/Taxonomy/index.js
+++ src/components/Taxonomy/index.js
@@ -5,8 +5,27 @@ import styles from './styles.module.css';
 import { ArcherContainer, ArcherElement } from 'react-archer';
 
 
-const rowStyle = { margin: '50px 0', display: 'flex', justifyContent: 'space-between', }
+const defaultSelected = 'tx3-thresholding'
 
+/**
+ * Swap the content of '#taxonomy-desc' with the content referenced by href.
+ * Change selected item in Taxonomy.
+ */
+function handleClick(id, href, e) {
+  e.preventDefault();
+
+  // Swap content
+  href = href.substring(1);
+  var newContent = document.getElementById(href).innerHTML;
+  document.getElementById("taxonomy-desc").innerHTML = newContent;
+
+  // Change selected item
+  var old_selection = document.getElementsByClassName(styles.taxonomyOn);
+  for (var i = 0; i < old_selection.length; i++) {
+    old_selection[i].classList.remove(styles.taxonomyOn);
+  }
+  document.getElementById(id).classList.add(styles.taxonomyOn);
+}
 
 /**
  * If the given text is an Array, join each item with <br />
@@ -22,13 +41,31 @@ function innerText() {
  * @returns list to pass to 'relations' param
  */
 function link() {
+  var color = document.documentElement.getAttribute('data-theme');
+  if (color == 'dark') {
+    color = '#D9D9D9';
+  }
+  else {
+    color = 'black';
+  }
+
+  var width = screen.width;
+  if (width <= 600) {
+    var sWidth = 1;
+    var curves = false;
+  }
+  else {
+    var sWidth = 2
+    var curves = true;
+  }
+
   var res = []
   for (var i = 0; i < arguments.length; i++) {
     res.push({
       targetId: arguments[i],
       targetAnchor: 'left',
       sourceAnchor: 'right',
-      style: { strokeColor: 'black', strokeWidth: 2, noCurves: 'true', svgContainerStyle: 'z-index: -1', },
+      style: { strokeColor: color, strokeWidth: sWidth, noCurves: curves, svgContainerStyle: 'z-index: -1', },
     })
   }
 
@@ -54,7 +91,7 @@ const Block = ({ id, relations, text }) => {
     <div>
       <ArcherElement id={id} relations={relations} >
         <div>
-        <p className={clsx('text--center no-margin')} >{textRes}</p>
+        <p className={styles.blockText} >{textRes}</p>
         </div>
       </ArcherElement>
     </div>
@@ -67,11 +104,20 @@ const Block = ({ id, relations, text }) => {
  * @returns HTML code to place inside a div.taxonomy-col
  */
 const ClickBlock = ({ id, href, text }) => {
+  var id_anchor = String(id).concat('_anchor')
+
+  if (id == defaultSelected) {
+    var classes = [styles.taxonomyLink, styles.taxonomyOn].join(' ')
+  }
+  else {
+    var classes = styles.taxonomyLink;
+  }
+
   return <div className={styles.taxonomyPad} >
-    <a className={styles.taxonomyLink} href={href}>
+    <a id={id_anchor} className={classes} href={href} onClick={(e) => handleClick(id_anchor, href, e)} onMouseOver={(e) => handleClick(id_anchor, href, e)}>
       <ArcherElement id={id}>
         <div>
-          <p className={clsx('text--center no-margin')} >{text}</p>
+          <p className={styles.blockText} >{text}</p>
         </div>
       </ArcherElement>
     </a>
@@ -83,7 +129,7 @@ export default function Taxonomy() {
   return (
     <div>
       <ArcherContainer strokeColor='black' >
-        <div style={rowStyle}>
+        <div className={styles.taxonomyRow}>
           <div className={styles.taxonomyCol} />
           <div className={styles.taxonomyCol} >
             <Block
@@ -105,7 +151,7 @@ export default function Taxonomy() {
             />
           </div>
         </div>
-        <div style={rowStyle}>
+        <div className={styles.taxonomyRow}>
           <div className={styles.taxonomyCol} >
             <Block
               id="tx1-skindetector"
@@ -138,7 +184,7 @@ export default function Taxonomy() {
             />
           </div>
         </div>
-        <div style={rowStyle}>
+        <div className={styles.taxonomyRow}>
           <div className={styles.taxonomyCol} />
           <div className={styles.taxonomyCol} >
             <ClickBlock
@@ -150,6 +196,12 @@ export default function Taxonomy() {
           <div className={styles.taxonomyCol} />
         </div>
       </ArcherContainer>
+      <div id="taxonomy-desc" className={clsx('col col--8 col--offset-2')} style={{minHeight: '220px'}} >
+        <p>
+        <b>Thresholding</b> approaches use <em>plain rules</em> to classify each pixel as either skin or non-skin. An example is the following.<br />
+        A given (Y,Cb,Cr) pixel is a skin pixel if 133&lt;=Cr&lt;=173 and 77&lt;=Cb&lt;=127
+        </p>
+      </div>
     </div>
   );
 }

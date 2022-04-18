0ef0608b80e67f3aa42c9340ebbe0e8ef0f3b02d
Michele Pozzi <123.mpozzi@gmail.com>
2022-04-19 00:07:14 +0200

    Add single datasets notes

--- src/components/Tables/utils.js
+++ src/components/Tables/utils.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip';
 
+let prevBg;
+let flag = false;
+
 export const Tooltip = ({children, id}) => (
   <ReactTooltip id={id} place="top" effect="solid" border borderColor="var(--ifm-color-primary)" >
     <div style={{maxWidth: 'min(70vw, 200px)'}} >
@@ -8,3 +11,40 @@ export const Tooltip = ({children, id}) => (
     </div>
   </ReactTooltip>
 )
+
+/**
+ * Change background color of the cells matching the given class
+ */
+export function handleOver(matchClass, color, e) {
+  e.preventDefault();
+  const cells = document.getElementsByClassName(matchClass);
+  // Change background
+  for (var i = 0; i < cells.length; i++) {
+    // the fist time save the initial background color
+    if (flag == false){
+      prevBg = cells[i].style.background;
+      flag = true;
+    }
+    cells[i].style.setProperty('background', color, 'important');
+  }
+  // Revert back to previous background
+  /*setTimeout(function() { 
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.setProperty('background', prevBg, 'important');
+    }
+  }, 5000);*/
+}
+
+/**
+ * Revert back to the initial background
+ */
+export function handleOut(matchClass, e) {
+  e.preventDefault();
+  const cells = document.getElementsByClassName(matchClass);
+  // Revert back to previous background
+  setTimeout(function() { 
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.setProperty('background', prevBg, 'important');
+    }
+  }, 500);
+}
